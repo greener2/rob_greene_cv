81bc3974b719fa71c274eb18318c3de7c487c074
Rob Greene <greener2@gmail.com>
2022-01-23 14:56:25 +0000

    Add placeholder partials for other sections, and autoload about-me section

--- rob_greene_cv/views.py
+++ rob_greene_cv/views.py
@@ -16,3 +16,23 @@ def about_me_partial():
 @bp.get("/prof-exp")
 def professional_exp_partial():
     return render_template("partials/prof-exp.html")
+
+
+@bp.get("/skills")
+def skills_partial():
+    return render_template("partials/skills.html")
+
+
+@bp.get("/education")
+def education_partial():
+    return render_template("partials/education.html")
+
+
+@bp.get("/hobbies")
+def hobbies_partial():
+    return render_template("partials/hobbies.html")
+
+
+@bp.get("/contact")
+def contact_partial():
+    return render_template("partials/contact.html")
